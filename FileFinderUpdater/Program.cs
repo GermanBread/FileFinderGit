48dc3de1e15d44f46b0b86f1e03ba25dcea679ba
Mathis <nd01@online.de>
2020-09-24 15:24:31 +0200

    cleanup

---
+++ FileFinderUpdater/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileFinderUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FileFinder updater");
+            Console.WriteLine("Made by GermanBread#9087");
+        }
+    }
+}
